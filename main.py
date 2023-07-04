37b60efe411dd71f3b5a2270f8e212901b6e26bc
etoprostobred <samoilowamary@yandex.ru>
2023-07-04 12:50:12 +0300

    Init Skillfactory

---
+++ main.py
@@ -0,0 +1,10 @@
+iter_obj = iter("Hello!")
+
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
+print(next(iter_obj))
